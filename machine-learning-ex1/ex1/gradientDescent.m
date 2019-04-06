dc7eb039c70fbc9e09b16c10ef053ba5133a3358
Alexander Olivero <alexander.t.olivero@gmail.com>
2019-04-06 08:14:32 -0400

    Fill out solution for gradient descent
    Do this to complete mandatory exercises

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -8,6 +8,11 @@ m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
+    temp0 = theta(1) - alpha * (1/m) * sum(X * theta - y);
+    temp1 = theta(2) - alpha * (1/m) * sum((X * theta - y) .* X(:,2));
+
+    theta(1) = temp0;
+    theta(2) = temp1;   
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
