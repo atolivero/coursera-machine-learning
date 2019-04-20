ff5059ae7ecddcb642da07059b0be9182305e02a
Alexander Olivero <alexander.t.olivero@gmail.com>
2019-04-20 15:37:36 -0400

    Add solution to compute regularized logistic regression
    Do this to complete the exercise

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,10 +17,9 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
-
-
-
+theta1 = [0 ; theta(2:size(theta), :)];
+J = ((-y)'*log(sigmoid(X*theta)) - (1-y)'*log(1-sigmoid(X*theta)))/m + lambda*(theta1'*theta1)/(2*m);
+grad = (X'*(sigmoid(X*theta) - y)+lambda*theta1)/m;
 
 % =============================================================
 
